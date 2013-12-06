938a90cc2d259150533748df879ee56a7c6d4392
RobinsonCampos <robinsoncampos@hotmail.es>
2013-12-06 15:09:33 -0400

    Trabajo
    Subir Nota

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,54 @@
+package com.example.subirnota;
+
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		Button boton = (Button) findViewById(R.id.button1);
+		boton.setOnClickListener(this);
 		
 		
 	}
+		
+		public void onClick(View arg0) {
+			if(arg0.getId()==findViewById(R.id.button1).getId()){
+				TextView texto1=(TextView)findViewById(R.id.textView1);
+				
+		Data dat = new Data();
+		ArrayList<Scrawl> dt = dat.getScrawlList();
+		
+		
+		for (Scrawl repite1:dt){
+			for (Scrawl repite2:dt){
+				for (Scrawl repite3:dt){
+			
+			texto1.setText(repite1.s+": "+repite1.i+"\n"+repite2.s+": "+repite3.i+"\n"+repite3.s+": "+repite2.i);
+			}
+				
+		
+			}
+		}
+			
+		}
+		
+	}
+		
+			
+	
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,4 +57,11 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-}
+//	@Override
+	//public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	
+	
+	}
+
